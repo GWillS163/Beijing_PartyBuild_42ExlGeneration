d4cdf4a2f562f2148cebc0c129e747fa3bd7b9b3
junqing_Gitkraken <gwills@163.com>
2022-08-19 23:49:44 +0800

    MS-SDE-Written
    Signed-off-by: junqing_Gitkraken <gwills@163.com>

---
+++ algorithmOfSummer_2022/WrittenFor2023CampusRecruitment/MS_SDE/car_sharing.py
@@ -0,0 +1,47 @@
+#  Author : Github: @GWillS163
+#  Time: $(Date)
+from pprint import pprint
+
+
+class TreeNode():
+    def __init__(self, val=0, children=None):
+        self.val = val
+        self.cost = 0
+        self.children = children
+
+
+def solution(A=[0, 1, 1], B=[1, 2, 3]):
+    pathDct = {}
+    costDct = {}
+    for a, b in zip(A, B):
+        if a not in pathDct:
+            pathDct[a] = [b]
+        else:
+            pathDct[a].append(b)
+        if b not in pathDct:
+            pathDct[b] = [a]
+        else:
+            pathDct[b].append(a)
+    # pprint(pathDct)
+    for roots in pathDct[0]:
+        costDct[roots] = recursionSumTree(pathDct, roots, [])
+    return sum(costDct.values())
+
+
+def recursionSumTree(pathDct, node, visited):
+    if node in visited:
+        return 0
+    visited.append(node)
+    cost = 0
+    if len(pathDct[node]) == 1:
+        cost = 1
+    else:
+        for node in pathDct[node]:
+            cost += recursionSumTree(pathDct, node, visited)
+    return cost
+
+
+if __name__ == '__main__':
+    print(solution())
+    print(solution([1, 1, 1, 9, 9, 9, 9, 7, 8],
+                    [2, 0, 3, 1, 6, 5, 4, 0, 0]))
--- forWorking/HuayunTechRPA/Exl_Opr_2022-8-18/main_xlwings.py
+++ forWorking/HuayunTechRPA/Exl_Opr_2022-8-18/main_xlwings.py
@@ -6,7 +6,6 @@ import xlwings as xw
 import pandas as pd
 
 
-
 class Excel_Operation():
     def __init__(self, surveyExlPath, scoreExlPath):
         self.surveyExlPath = surveyExlPath
@@ -27,42 +26,48 @@ class Excel_Operation():
         self.app4Attach1.quit()
         self.app4Attach2.quit()
 
-    def genSheet1_surveyWithoutWeight(self, departmentLst, newSheetName, surveySheetName='测试问卷', copy_scope="A1:J32", titleStart="K1"):
+    def genSheet1_surveyWithoutWeight(self, surveySht, departmentLst,
+                                      newSheetName,
+                                      copy_scope="A1:J32",
+                                      titleStart="K1"):
         """
         generate sheet1 of the surveyExl, which is the survey without weight
+        :param departmentLst:
+        :param surveySht:
         :param newSheetName:
-        :param surveySheetName:
         :param copy_scope:
         :param titleStart:
         :return:
         """
-        # open the sheet named "测试问卷" of surveyPath
-        surveySht = self.surveyExl.sheets[surveySheetName]
-        newSht = self.surveyExl.sheets.add(newSheetName)
+        sht1 = self.surveyExl.sheets.add(newSheetName)
 
-        # Step1: copy left column the surveySht to newSht
-        newSht.range(copy_scope).value = \
+        # Step1: copy left column the surveySht to sht1
+        sht1.range(copy_scope).value = \
             surveySht.range(copy_scope).value
 
         # Step2: place title
-        newSht.range(titleStart).value = departmentLst
+        sht1.range(titleStart).value = departmentLst
         # get merge info
         startRow = titleStart[1]
-        endLetter = chr(ord(titleStart[0]) + len(departmentLst))
+        endLetter = chr(ord(titleStart[0]) + len(departmentLst[1]) - 1)
         # merge K1:L1 cells
-        newSht.range(f"{titleStart}:{endLetter}{startRow}").merge()
+        # TODO: record index position
+        sht1.range(f"{titleStart}:{endLetter}{startRow}").merge()
 
         # save
         self.surveyExl.save("test.xlsx")
 
-    def genSheet2_surveyWithWeight(self, departmentLst):
+    def genSheet2_surveyWithWeight(self, surveySht, departmentLst, sht2Name):
         """
         generate sheet2 of the surveyExl, which is the survey with weight
         :return:
         """
-        pass
+        # Area 1: copy partial scope
+        # department:
 
-    # make the function private
+        # Area 2:
+        # department
+
     def __setAllDepartmentTitle(self, departmentScope="A1:F10"):
         """
         set the departments title from the surveyExl, can be inserted
@@ -93,32 +98,45 @@ class Excel_Operation():
         for layer3 in departmentDict:
             for layer2 in departmentDict[layer3]:
                 if result_lst[0].__len__() == 0:
-                    result_lst[0].append(layer3)
+                    result_lst[0].append(layer2)
                 else:
                     result_lst[0].append("")
-                result_lst[1].append(layer2)
+                result_lst[1].append(layer3)
             result_lst[0].append("")
             result_lst[1].append(other)
             result_lst[0].append("")
             result_lst[1].append(avg)
         return result_lst
 
-    def genOneDepartFile(self, departmentLst):
+    def genOneDepartFile(self, surveySht, departmentLst,  copy_scope="A1:J32", titleStart="K1", sht1Name="调研分数（未加权）", sht2Name="调研分数（加权）"):
         """
         generate one department file
+        :param surveySht:
+        :param titleStart:
+        :param copy_scope:
         :param departmentLst:
         :return:
         """
-        self.genSheet1_surveyWithoutWeight(departmentLst, surveySheetName="测试问卷", copy_scope="A1:J32", titleStart="K1")
-        self.genSheet2_surveyWithWeight(departmentLst)
+        # open the sheet named "测试问卷" of surveyPath
+        # TODO: 基础功能OK, 还差一个合并单元格的功能，颜色等演示。
+        self.genSheet1_surveyWithoutWeight(surveySht, departmentLst, sht1Name, copy_scope=copy_scope, titleStart=titleStart)
 
-    def genMultiDepartFile(self, ):
-        pass
+        # TODO: 两个area 都没实现
+        self.genSheet2_surveyWithWeight(surveySht, departmentLst, sht2Name)
 
-    def genSingleDepartFile(self):
+    def genAllDepartFile(self):
+        """
+        generate all department file, Summarization and Single Department
+        :return:
+        """
+        allDepartmentLst = [[], []]
         for department in self.departmentDict:
             departmentLst = self.__convertDict2Lst(department)
+            allDepartmentLst[0] += departmentLst[0]
+            allDepartmentLst[1] += departmentLst[1]
+
             self.genOneDepartFile(departmentLst)
+        self.genOneDepartFile(allDepartmentLst)
 
     def run(self):
         """
@@ -126,24 +144,30 @@ class Excel_Operation():
         :return:
         """
         # 1. get the all department title
-        # 2. get level 2 index columns
-        # 3. get level 3 index columns
-        departmentLst = []
         self.__setAllDepartmentTitle()
+        surveySht = self.surveyExl.sheets["测试问卷"]
+        # TODO:3. get level 3 index columns
+        self.level3_index = surveySht.range("A1:J32")
+        # TODO:2. get level 2 index columns
+        self.level4_index = surveySht.range("A1:B32")
+
+        # test, 获得单个部门
+        departmentLst = []
         for key in self.departmentDict:
             if "二级部门" in key:
                 continue
             print(key, self.departmentDict[key])
             departmentLst = self.__convertDict2Lst({key: self.departmentDict[key]})
             break
-        self.genSheet1_surveyWithoutWeight(departmentLst, "调研分数（未加权）", "测试问卷", "A1:J32")
+        print("写入 单个文件：departmentLst:", departmentLst)
+        self.genOneDepartFile(surveySht, departmentLst)
 
         self.close_excel()
         print("All done")
 
 
 if __name__ == '__main__':
-    surveyExlPh = r"D:\work\考核RPA_Exl\附件1：【测试问卷】中国移动北京公司2021年度党建工作成效调研—20220816.xlsx"
-    scoreExlPh = r"D:\work\考核RPA_Exl\附件2：党办调研问卷测试-8.15答题结果_20220815.xlsx"
+    surveyExlPh = "D:\work\考核RPA_Exl\Input\附件1：【测试问卷】中国移动北京公司2021年度党建工作成效调研—20220816.xlsx"
+    scoreExlPh = "D:\work\考核RPA_Exl\Input\附件2：党办调研问卷测试-8.15答题结果_20220815.xlsx"
 
     Excel_Operation(surveyExlPh, scoreExlPh).run()
