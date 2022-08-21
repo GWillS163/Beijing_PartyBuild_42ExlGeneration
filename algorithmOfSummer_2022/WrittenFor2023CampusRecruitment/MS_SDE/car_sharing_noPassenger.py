93c64c92504ba2627db5521ea63d2eb53d1d551a
junqing_Gitkraken <gwills@163.com>
2022-08-21 21:57:26 +0800

    MS-SDE-Written _ updated
    Signed-off-by: junqing_Gitkraken <gwills@163.com>

--- algorithmOfSummer_2022/WrittenFor2023CampusRecruitment/MS_SDE/car_sharing_noPassenger.py
+++ algorithmOfSummer_2022/WrittenFor2023CampusRecruitment/MS_SDE/car_sharing_noPassenger.py
@@ -37,4 +37,4 @@ def recursionSumTree(pathDct, node, visited):
 if __name__ == '__main__':
     print(solution())
     print(solution([1, 1, 1, 9, 9, 9, 9, 7, 8],
-                    [2, 0, 3, 1, 6, 5, 4, 0, 0]))
+                   [2, 0, 3, 1, 6, 5, 4, 0, 0]))
